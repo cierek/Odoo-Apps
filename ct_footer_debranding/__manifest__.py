f5c0da8c6f68b38477bf80bb980bad3be3d3616d
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2019-03-09 18:50:46 +0000

    Add files via upload
    initial commit

---
+++ ct_footer_debranding/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2019 Piotr Cierkosz <piotr.w.cierkosz@gmail.com>
+{
+    'name': "Footer Debranding for Website",
+    'version': "1.0",
+    'author': "Piotr Cierkosz",
+    'website': "https://www.cier.tech",
+    'category': "Website",
+    'data': ["views/ct_footer_debranding.xml"],
+    'images': ['images/thumbnail.png'],
+    'depends' : ['website'],
+    'installable' : True,
+    'description' : "Remove Odoo branding from the footer",
+    'license': 'Other proprietary',
+    'summary': 'Remove Odoo branding from the footer',
+
+}
---
+++ ct_footer_debranding/doc/changelog.rst
@@ -0,0 +1,8 @@
+.. _changelog:
+
+Changelog
+=========
+
+`Version 1.0 (2019)`
+-------------------------
+- First release for Odoo 12.0.
