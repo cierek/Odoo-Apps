2ab40842aa6f700993386b5f2d47a800eef3139a
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2019-03-09 18:56:07 +0000

    Add files via upload

---
+++ l10n_ph_country_states/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# (C) Piotr Cierkosz
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Philippines Country States',
+    'category': 'Localisation',
+    'version': '1.0',
+    'author': 'Piotr Cierkosz',
+    'data': [
+        'data/res_country_states.xml',
+    ],
+    'license': 'AGPL-3',
+    'installable': True,
+    'description' : "PProvinces of the Philippines",
+    'images': ['images/thumbnail.png'],
+    'summary': 'Provinces of the Philippines',
+    'website' : "https://www.cier.tech",
+}
