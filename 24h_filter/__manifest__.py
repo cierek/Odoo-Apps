58b034f74f43c922feeff7f1690c84f986133f63
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2019-03-09 19:05:57 +0000

    Add files via upload

---
+++ 24h_filter/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : "24h filter",
+    'version' : "1.1",
+    'author' : "Piotr Cierkosz",
+    'images': ['images/thumbnail.png'],
+    'depends' : ['sale', 'stock', 'account', 'purchase'],
+    'installable' : True,
+    'description' : "This module adds 24h filter to Sales, Inventory and Accounting",
+    'website': "www.cier.tech",
+    'data': ['views/so_f_24.xml', 'views/po_f_24.xml', 'views/inv_f_24.xml', 'views/acc_f_24.xml', 'views/pay_f_24.xml'],
+    'category': 'Extra Tools',
+    'license': 'Other proprietary',
+    'summary': 'This module adds 24h filter to Sales, Inventory and Accounting',
+
+}
---
+++ 24h_filter/doc/changelog.rst
@@ -0,0 +1,8 @@
+.. _changelog:
+
+Changelog
+=========
+
+`Version 1.0 (2018)`
+-------------------------
+- First release for Odoo 11.0.
