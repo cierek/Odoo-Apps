4ccf8ff93f69bfbee156aee6e40418fa138c17dd
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2019-03-09 18:55:27 +0000

    Add files via upload

---
+++ opportunities_on_so_list/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : "Opportunity on Sales Orders list",
+    'version' : "1.1",
+    'images': ['images/thumbnail.png'],
+    'author' : "Piotr Cierkosz",
+    'category': 'Sales',
+    'license': 'Other proprietary',
+    'depends' : ['sale', 'crm'],
+    'installable' : True,
+    'description' : "This module adds opportunities to the orders list view",
+    'website': "www.cier.tech",
+    'summary': 'This module allows users to see the connected opportunity on the orders list view',
+
+    'data': ['views/so_tree_views.xml',],
+
+}
