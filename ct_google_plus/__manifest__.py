efcf0edbad2f32e7bc8e5010bae95755ca630a0a
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2019-03-09 18:52:11 +0000

    Add files via upload

---
+++ ct_google_plus/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2018 Piotr Cierkosz <piotr.w.cierkosz@gmail.com>
+{
+    'name': "Remove Google+ from Odoo",
+    'version': "1.0",
+    'author': "Piotr Cierkosz",
+    'website': "www.cier.tech",
+    'category': "Website",
+    #comment or remove company_form_googleplus0 below if you are not using mass mailing
+    'data': ["views/company_form_googleplus.xml",
+    #"views/company_form_googleplus0.xml",
+    ],
+    'images': ['images/thumbnail.png'],
+    'depends' : ['website',
+    #comment or remove mass_mailing below if you are not using mass mailing
+    #'mass_mailing',
+    ],
+    'installable' : True,
+    'description' : "This module removes Google+ field from Company form",
+    'license': 'Other proprietary',
+    'summary': 'This module removes Google+ field from Company form',
+}
---
+++ ct_google_plus/doc/changelog.rst
@@ -0,0 +1,16 @@
+.. _changelog:
+
+Changelog
+=========
+`Version 1.1.3 (30-01-2019)`
+- Imrpoved code in product view. Now only attributes are changed instead of replacing the entire view
+`Version 1.1.2 (2019)`
+
+Fixed issues with Add to cart if activated
+
+`Version 1.1.1 (2019)`
+-------------------------
+fixed issue with "Product unavailable" - commented on line 26
+`Version 1.0 (2018)`
+-------------------------
+- First release for Odoo 12.0.
