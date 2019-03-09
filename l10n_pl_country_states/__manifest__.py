d67806120659b39a3c914f19270da1bd43e2fc1b
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2019-03-09 18:56:44 +0000

    Add files via upload

---
+++ l10n_pl_country_states/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# (C) Piotr Cierkosz
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Polish Country States',
+    'category': 'Localisation/Europe',
+    'version': '1.1',
+    'author': 'Piotr Cierkosz',
+    'data': [
+        'data/res_country_states.xml',
+    ],
+    'license': 'AGPL-3',
+    'installable': True,
+    'description' : "Polish Country States - adds Voivodeships of Poland",
+    'images': ['images/thumbnail.png'],
+    'summary': 'Polish Country States - adds Voivodeships of Poland',
+    'website' : "https://www.cier.tech",
+}
---
+++ l10n_pl_country_states/doc/changelog.rst
@@ -0,0 +1,15 @@
+.. _changelog:
+
+Changelog
+=========
+1.1 (07-AUG-18)
+- codes compatible with ISO 3166-2:PL
+- description updated
+1.0.1
+- thumbnail
+- icon
+- description updated
+
+`Version 1.0 (2018)`
+-------------------------
+- First release for Odoo 12.0
