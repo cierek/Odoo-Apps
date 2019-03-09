dbf658ebc7261a9841d32294eca11db45af7ce88
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2019-03-09 18:53:13 +0000

    Add files via upload

---
+++ force_invoice_name/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Piotr Cierkosz (https://www.cier.tech)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name' : "Force Invoice Name",
+    'version' : "12.0.1.0",
+    'images': ['images/thumbnail.png'],
+    'author' : "Piotr Cierkosz",
+    'category': 'Accounting & Finance',
+    'license': 'AGPL-3',
+    'depends' : ['account'],
+    'installable' : True,
+    'description' : "Allows to force the Invoice Name",
+    'website': "https://www.cier.tech",
+    'summary': 'Allows to force the Invoice Name',
+    'data': [
+    'views/account_invoice_force_number_customer.xml',
+    'views/account_invoice_force_number_vendor.xml',
+    ],
+    'live_test_url': 'https://youtu.be/_B-V-5Bz4gY',
+}
---
+++ force_invoice_name/doc/changelog.rst
@@ -0,0 +1,9 @@
+.. _changelog:
+
+Changelog
+=========
+
+`Version 1.0 (2018)`
+-------------------------
+- First release for Odoo 12.0
+- Views are separate to keep the Single Responsibility
