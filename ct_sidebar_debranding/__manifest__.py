786f2cf228d7d1cc74faff6c8175783918e6ae21
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2019-03-14 20:52:02 +0000

    Add files via upload

---
+++ ct_sidebar_debranding/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2019 Piotr Cierkosz <piotr.w.cierkosz@gmail.com>
+{
+    'name': "Sidebar Debranding for Website",
+    'version': "1.0",
+    'author': "Piotr Cierkosz",
+    'website': "https://www.cier.tech",
+    'category': "Website",
+    'data': ["views/ct_sidebar_debranding.xml"],
+    'images': ['images/thumbnail.png'],
+    'depends' : ['website'],
+    'installable' : True,
+    'description' : "Remove Odoo branding from the Sidebar",
+    'license': 'Other proprietary',
+    'summary': 'Remove Odoo branding from the Sidebar',
+
+}
