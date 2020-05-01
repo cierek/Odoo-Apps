f0b29f89add6bc452bf5d2ca6994cfa5eac56250
Piotr Cierkosz <piotr.w.cierkosz@igmail.com>
2020-05-01 16:13:52 +0100

    footer

---
+++ ct_your_footer/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2020 Piotr Cierkosz <info@cier.tech>
+{
+    'name': "Your company footer for Website",
+    'version': "12.0.1.0",
+    'author': "Piotr Cierkosz",
+    'website': "https://www.cier.tech",
+    'category': "Website",
+    'data': ["views/ct_footer_debranding.xml"],
+    'images': ['images/thumbnail.png'],
+    'depends' : ['website'],
+    'installable' : True,
+    'description' : "Add your information to the footer",
+    'license': 'Other proprietary',
+    'summary': 'Add your information to the footer',
+    'price': 00.0,
+    'currency': 'EUR',
+    'application': True,
+}
---
+++ ct_your_footer/doc/changelog.rst
@@ -0,0 +1,8 @@
+.. _changelog:
+
+Changelog
+=========
+
+`Version 1.0 (01-05-2020)`
+-------------------------
+- First release for Odoo 12.0.
