d08cae730f654f200631c6695ba5484d9f9b341d
Piotr Cierkosz <piotr.w.cierkosz@igmail.com>
2020-06-20 21:55:30 +0100

    hide price theme laze

---
+++ hide_price_shop_laze/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2018 Piotr Cierkosz <piotr.w.cierkosz@gmail.com>
+{
+    'name': "Hide Price in Shop for Theme Laze",
+    'version': "10.0.1.0",
+    'author': "Piotr Cierkosz",
+    'website': "www.cier.tech",
+    'category': "Website",
+    'data': ["views/kanban_price_hide0.xml"],
+    'images': ['images/thumbnail.png'],
+    'depends' : ['website_sale', 'hide_price_shop', 'theme_laze'],
+    'installable' : True,
+    'description' : "This module hides the price if user is not logged in. It also replaces Add to Cart button with Log in to see the price",
+    'license': 'Other proprietary',
+    'summary': 'Hide price of the products from not logged in users',
+    'price': 0,
+    'currency': 'EUR',
+}
